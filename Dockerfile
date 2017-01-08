bea60f7b41001f6e33b11249c560f8b1aaab0d23
Todd Lunter <tlunter@gmail.com>
2017-01-07 19:03:07 -0500

    Pre-compile and request immediatebly

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,6 @@ FROM node:5.6.0
 ADD . /app
 WORKDIR /app
 RUN npm install
+RUN npm run build:min:js
 EXPOSE 3000
 CMD ["node","index.js"]
---
+++ src/app.js
@@ -0,0 +1,135 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+const initialStartingDate = new Date(2016, 4, 29);
+const scheduleLength = 7 * 4;
+const morningFeedingTime = 6 * 60 + 45; // 6:45am
+const afternoonFeedingTime = 17 * 60; // 5pm
+const morningFeedingTimeOffset = morningFeedingTime - 2 * 60;
+const afternoonFeedingTimeOffset = afternoonFeedingTime - 2 * 60;
+
+const chicken = { name: "Chicken Frick 'Azee", color: "#ffde38" },
+      turkey = { name: "Fowl Ball", color: "#a57232" },
+      pumpkin = { name:"Funk in the Trunk", color: "#098ed0" },
+      lamb = { name: "Lamb Burgini", color: "#17ae15" },
+      salmon = { name: "Goldie Lox", color: "#efaf39" };
+
+const weeklySchedule = [
+  [chicken, turkey],
+  [chicken, chicken],
+  [turkey, chicken],
+  [chicken, turkey],
+  [chicken, chicken],
+  [turkey, chicken],
+  [chicken, pumpkin],
+];
+
+const isThursday = function(date) {
+  return date.getDay() == 4;
+};
+
+const thursdayMeals = function(dayOfSchedule) {
+  const week = parseInt(dayOfSchedule / 7);
+
+  if (week == 1) {
+    return [lamb, chicken];
+  } else if (week == 3) {
+    return [salmon, chicken];
+  } else {
+    return weeklySchedule[4];
+  }
+};
+
+const millisToDays = function(millis) {
+  return millis / (86400 * 1000)
+};
+
+const findDayOfSchedule = function(date) {
+  return parseInt(millisToDays(date - initialStartingDate) % scheduleLength);
+};
+
+const dateToMinutes = function(date) {
+  return date.getHours() * 60 + date.getMinutes();
+};
+
+var App = React.createClass({
+  getInitialState: function() {
+    return {
+      date: new Date(),
+      loading: true,
+      fed: false,
+    };
+  },
+  componentWillMount: function() {
+    setInterval(this.updateApp, 2000);
+    this.updateApp()
+  },
+  clickedFed: function() {
+    fetch('/fed', {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ fed: true })
+    })
+    .then(this.updateApp);
+  },
+  updateApp: function() {
+    const nextDate = new Date();
+
+    fetch('/fed')
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(json) {
+        this.setState({ date: nextDate, fed: json.fed, loading: false });
+      }.bind(this));
+  },
+  meals: function() {
+    const currentDay = findDayOfSchedule(this.state.date);
+
+    if (isThursday(this.state.date)) {
+      return thursdayMeals(currentDay);
+    } else {
+      return weeklySchedule[currentDay % 7];
+    }
+  },
+  renderMeal: function(meal) {
+    return (
+      <div>
+        <div className="meal">{meal.name}</div>
+        <button
+          className="button"
+          onClick={this.clickedFed}
+          disabled={this.state.fed}>
+            Fed
+        </button>
+      </div>
+    );
+  },
+  renderLoading: function() {
+    return (<div className="loading">Loading!</div>);
+  },
+  render: function() {
+    const meals = this.meals();
+    const currentMinutes = dateToMinutes(this.state.date);
+    let meal;
+
+    if (currentMinutes <= morningFeedingTimeOffset || currentMinutes < afternoonFeedingTimeOffset) {
+      meal = meals[0];
+    } else {
+      meal = meals[1];
+    }
+
+    const backgroundColor = (this.state.fed || this.state.loading) ? '#ececec' : meal.color;
+
+    return (
+      <div className="app" style={{background: backgroundColor}}>
+        {this.state.loading ? this.renderLoading() : this.renderMeal(meal)}
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(<App />, document.querySelector("#content"));
